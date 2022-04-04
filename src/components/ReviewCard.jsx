3aba898878cc0584c4cdfe05a19495fc1bcf7106
Tariqul Islam <usukuk@gmail.com>
2022-04-04 17:36:01 +0600

    fix netlify page-not-found on reload

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -3,7 +3,7 @@ import "../styles/review-card.css";
 
 const ReviewCard = ({ name, text, rating }) => {
   return (
-    <div className="card">
+    <div className="card h-100">
       <div className="card-body">
         <h5 className="card-title text-success"> &#8594; {name}</h5>
         <blockquote className="review-text">&ldquo;{text}&rdquo;</blockquote>

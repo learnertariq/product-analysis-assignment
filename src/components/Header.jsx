102455faf700b3d85a163c078965f830def4a72b
Tariqul Islam <usukuk@gmail.com>
2022-04-05 10:57:16 +0600

    add active link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "../styles/header.css";
 
 const Header = () => {
   return (
-    <header className="header d-flex justify-content-center gap-4">
-      <span>
-        <Link to="/home">Home</Link>
-      </span>
-      <span>
-        <Link to="/reviews">Reviews</Link>
-      </span>
-      <span>
-        <Link to="/dashboard">Dashboard</Link>
-      </span>
-      <span>
-        <Link to="/blogs">Blogs</Link>
-      </span>
-      <span>
-        <Link to="/about">About</Link>
-      </span>
+    <header className="header">
+      <nav className="d-flex justify-content-center gap-4">
+        <span>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "link-active" : "link")}
+          >
+            Home
+          </NavLink>
+        </span>
+        <span>
+          <NavLink
+            to="/reviews"
+            className={({ isActive }) => (isActive ? "link-active" : "link")}
+          >
+            Reviews
+          </NavLink>
+        </span>
+        <span>
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) => (isActive ? "link-active" : "link")}
+          >
+            Dashboard
+          </NavLink>
+        </span>
+        <span>
+          <NavLink
+            to="/blogs"
+            className={({ isActive }) => (isActive ? "link-active" : "link")}
+          >
+            Blogs
+          </NavLink>
+        </span>
+        <span>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "link-active" : "link")}
+          >
+            About
+          </NavLink>
+        </span>
+      </nav>
     </header>
   );
 };
